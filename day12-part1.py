731539508f4028f445388e7122029633fd67f4af
Ted Romer <tromer@gmail.com>
2020-12-21 02:46:11 +0000

    wip

--- day12-part1.py
+++ day12-part1.py
@@ -1,7 +1,17 @@
 #!/home/tromer/advent-of-code-2020/venv/bin/python3
 
+import sys
+
+class Boat:
+  def __init__(self):
+    self.heading = 90
+    self.x = 0
+    self.y = 0
+
+  def command(self)
 def main():
     print("hi there")
+    for line in sys.stdin:
 
 if __name__ == "__main__":
     main()
