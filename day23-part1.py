becd83b257081d7af7be4675985db6ea85e59a40
Ted Romer <tromer@gmail.com>
2020-12-23 05:31:20 +0000

    day 23 part 1

--- day23-part1.py
+++ day23-part1.py
@@ -5,10 +5,34 @@ import sys
 
 from collections import defaultdict
 
-def main():
+MOVES = 100
 
-  for line in sys.stdin:
-    line = line.strip()
+def advance(cups, current):
+  print(cups, current) # , type(cups), type(current))
+  # first off, re-arrange so that current = 0
+  cups = cups[current:] + cups[0:current]
+  current = 0
+  saved = cups[1:4]
+  cups = [cups[0]] + cups[4:]
+  destination = 9 if cups[current] == 1 else cups[current] - 1
+  print(f'dest={destination}')
+  while destination in saved:
+    destination = 9 if destination == 1 else (destination - 1)
+    print(f'dest={destination}')
+  dest_index = cups.index(destination)
+  cups = cups[0:dest_index+1] + saved + cups[dest_index+1:]
+  current = 1
+  return cups, current
+
+def main():
+  cups = [int(x) for x in sys.stdin.readline().strip()]
+  current = 0
+  for i in range(MOVES):
+    cups, current = advance(cups, current)
+  one_pos = cups.index(1)
+  print(cups)
+  print(''.join([str(x) for x in cups[one_pos+1:] + cups[0:one_pos]]))
+
 
 if __name__ == "__main__":
     main()
