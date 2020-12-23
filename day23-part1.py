492fb81b6b35d3acc46cac1b613db2bc36e624b1
Ted Romer <tromer@gmail.com>
2020-12-23 05:05:18 +0000

    wip

---
+++ day23-part1.py
@@ -0,0 +1,14 @@
+#!/home/tromer/advent-of-code-2020/venv/bin/python3
+
+import re
+import sys
+
+from collections import defaultdict
+
+def main():
+
+  for line in sys.stdin:
+    line = line.strip()
+
+if __name__ == "__main__":
+    main()
