977af252d5942d94485d224fec7cf0a68eb1f455
Ted Romer <tromer@gmail.com>
2020-12-23 03:12:28 +0000

    part 1

--- day21-part1.py
+++ day21-part1.py
@@ -10,6 +10,7 @@ def main():
   allergen_ingredient_sets = defaultdict(list)
   all_allergens = set()
   all_ingredients = set()
+  foods = []
   for line in sys.stdin:
     line = line.strip()
     ingr_str, allergen_str = re.search(r'^(.*) \(contains (.*)\)$', line).groups()
@@ -17,27 +18,31 @@ def main():
     all_ingredients |= set(ingredients)
     allergens = allergen_str.split(', ')
     all_allergens |= set(allergens)
+    foods.append((ingredients, allergens))
     for allergen in allergens:
       allergen_ingredient_sets[allergen].append(set(ingredients))
   print(f'all ingr {all_ingredients}')
   print(f'all allergens {all_allergens}')
-  for allergen in allergen_ingredient_sets.keys():
-    all_ingredients = set(allergen_ingredient_sets[allergen][0])
-    candidate_ingredients = set(allergen_ingredient_sets[allergen][0])
-    for ingredient_set in allergen_ingredient_sets[allergen][1:]:
-      candidate_ingredients &= ingredient_set
-      all_ingredients |= ingredient_set
-    non_allergenic_ingredients = all_ingredients - candidate_ingredients
-    print(f'no {allergen} in {non_allergenic_ingredients}')
 
+  non_allergenic_ingredients = all_ingredients.copy()
   for allergen in allergen_ingredient_sets.keys():
     candidate_ingredients = all_ingredients.copy()
-    print(f'{allergen} is in one of {candidate_ingredients}')
+    #print(f'{allergen} is in one of {candidate_ingredients}')
     for ingredient_set in allergen_ingredient_sets[allergen]:
-      print(f'intersect with {ingredient_set}')
+      #print(f'intersect with {ingredient_set}')
       candidate_ingredients &= ingredient_set
-      print(f'{allergen} is in one of {candidate_ingredients}')
+      #print(f'{allergen} is in one of {candidate_ingredients}')
     print(f'{allergen} is in one of {candidate_ingredients}')
+    non_allergenic_ingredients -= candidate_ingredients
+  print(f'non allergenic {non_allergenic_ingredients}')
+  count = 0
+  for food in foods:
+    for ingredient in food[0]:
+      if ingredient in non_allergenic_ingredients:
+        count += 1
+  print(count)
+
+  
     
     
 
