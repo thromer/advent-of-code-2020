80c6f6567efc202da5101b0fba2fba79e92a856a
Ted Romer <tromer@gmail.com>
2020-12-25 19:34:28 -0800

    ugly day19 part2

--- day19-part3.py
+++ day19-part3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# grumble ... 315 is too high !
+# not 300 (just random was hoping they let me do binary search :(
+
 import re
 import sys
 
@@ -9,7 +12,8 @@ changes
 8 -> 42
 11 -> 42 31
 
-we have
+to
+
 0 -> 8 11
 8 -> 42 | 42 8
 11 -> 42 31 | 42 11 31
@@ -18,6 +22,33 @@ seems like
 - start with 1-infinity 42s (0-infinity from 8, 1 from 11)
 - end with 1-infinity 31s
 instead of just 1 of each
+
+so the 8 part can be
+42
+42 8 
+42 42
+42 42 42
+...
+
+and the 11 part can be
+42 31
+42 42 31 31
+...
+
+so I think 2 or more 42s
+followed by 1 or more 31s
+
+so there are quite a variety
+e.g. >=2 42s 1 31
+>=3 42s 2 31s
+etc.
+
+
+nope i made a mistake (thanks solution megathread)
+0 -> 8 11
+
+8 still looks to me like any number of 42s
+11 is equal number of 42s and 31s
 """
 
 class Rules:
@@ -68,8 +99,10 @@ def main():
   regex = rules.expand_rule('0')
   print(regex)
 
-  regex2 = (rules.expand_rule('42') + '+' +
-            rules.expand_rule('31') + '+')
+  rule42 = rules.expand_rule('42')
+  rule31 = rules.expand_rule('31')
+  regex2 = ('(' + rule42+')' + '(' + rule42  + ')+' +
+            '(' + rules.expand_rule('31') + ')+')
   print(regex2)
 
   count = 0
@@ -79,15 +112,30 @@ def main():
     #if good:
     #  print(message)
   print(count)
+
+  count1 = 0
+  regex = '^' +  rule42 + '{2}' + rule31 + '{1}$'
+  for message in messages:
+    if re.search(regex, message):
+      count1 += 1
+  print(count1)
+
+
+  regexes = []
   
+  for n31 in range(1, 20):
+    for n42 in range(n31+1, n31+20):
+      # print(f'{n42=} {n31=}')
+      regexes.append(f'^{rule42}{{{n42}}}{rule31}{{{n31}}}$')
+      
   count2 = 0
   for message in messages:
-    good = re.search('^' + regex2 + '$', message)
-    count2 += 1 if good else 0
-    #if good:
-    #  print(message)
+    for r in regexes:
+      if re.search(r, message):
+        count2 += 1
+        break
   print(count2)
-    
+
 
 if __name__ == "__main__":
     main()
